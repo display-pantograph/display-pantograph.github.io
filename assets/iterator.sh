a66cdd9baf93b8b01da746f70033128db10eb2c1
simonthi <49315922+simonthi@users.noreply.github.com>
2023-08-30 17:33:31 +0200

    Using printf, not echo

--- assets/iterator.sh
+++ assets/iterator.sh
@@ -2,7 +2,7 @@ for file in `ls -tU`; do
     if [[ $file =~ .*\.(png|jpeg|jpg) ]]; then 
         for file2 in `ls -tU`; do
             if [[ $file2 =~ .*\.(txt|rtf) && "${file%.*}" == "${file2%.*}" ]]; then
-                echo "---\nimg: assets/${file}\ntxt: \"`cat ${file2}`\"\n---" > ../posts/${file%.*}.md
+                printf "---\nimg: assets/${file}\ntxt: \"`cat ${file2}`\"\n---" > ../posts/${file%.*}.md
                 echo "Bingo"
                 break
             else
@@ -10,7 +10,7 @@ for file in `ls -tU`; do
             fi
         done
     elif [[ $file =~ .*\.(mp4) ]]; then 
-        echo "---\nvid: assets/${file}\n---" > ../posts/${file%.*}.md
+        printf "---\nvid: assets/${file}\n---" > ../posts/${file%.*}.md
     else
         echo $file "is neither video nor image, posts not written"
     fi
