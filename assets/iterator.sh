82a456269eee5eab57980b391f4629fc9f271bf3
simonthi <github@simonthiefes.de>
2023-10-05 09:21:34 +0200

    echo without -e

--- assets/iterator.sh
+++ assets/iterator.sh
@@ -2,15 +2,15 @@ for file in `ls -tU`; do
     if [[ $file =~ .*\.(png|jpeg|jpg) ]]; then 
         for file2 in `ls -tU`; do
             if [[ $file2 =~ .*\.(txt|rtf) && "${file%.*}" == "${file2%.*}" ]]; then
-                echo -e "---\nimg: assets/${file}\ntxt: \"`cat ${file2}`\"\n---" > ../posts/${file%.*}.md
+                echo "---\nimg: assets/${file}\ntxt: \"`cat ${file2}`\"\n---" > ../posts/${file%.*}.md
                 echo "Bingo"
                 break
             else
-                echo -e "---\nimg: assets/${file}\n---" > ../posts/${file%.*}.md
+                echo "---\nimg: assets/${file}\n---" > ../posts/${file%.*}.md
             fi
         done
     elif [[ $file =~ .*\.(mp4) ]]; then 
-        echo -e "---\nvid: assets/${file}\n---" > ../posts/${file%.*}.md
+        echo "---\nvid: assets/${file}\n---" > ../posts/${file%.*}.md
     else
         echo $file "is neither video nor image, posts not written"
     fi
