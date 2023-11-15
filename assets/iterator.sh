562d20f9edd703b43a68cc99c563f5d7c949228d
simonthi <github@simonthiefes.de>
2023-11-15 12:51:11 +0100

    Text for images

--- assets/iterator.sh
+++ assets/iterator.sh
@@ -11,7 +11,15 @@ for file in `ls -tU`; do
             fi
         done
     elif [[ $file =~ .*\.(mp4) ]]; then 
-        echo -e $"---\nvid: assets/${file}\n---" > ../posts/${file%.*}.md
+        for file2 in `ls -tU`; do
+            if [[ $file2 =~ .*\.(txt|rtf) && "${file%.*}" == "${file2%.*}" ]]; then
+                echo -e $"---\nvid: assets/${file}\ntxt: \"`cat ${file2}`\"\n---" > ../posts/${file%.*}.md
+                echo "Bingo"
+                break
+            else
+                echo -e $"---\nvid: assets/${file}\n---" > ../posts/${file%.*}.md
+            fi
+        done
     else
         echo $file "is neither video nor image, posts not written"
     fi
